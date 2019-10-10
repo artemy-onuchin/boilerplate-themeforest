d7607052e8c32e54d6a523b6fe82640c2dd6b523
Artemiy <artemonuchin@gmail.com>
2019-10-10 18:47:43 +0700

    Install and configured sass. Style files.
    npm install --save-dev gulp-sass gulp-autoprefixer gulp-concat-css gulp-group-css-media-queries gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 
 // Plugins
 let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concatCss = require('gulp-concat-css'),
+    gcmq = require('gulp-group-css-media-queries'),
+    cleanCSS = require('gulp-clean-css'),
     pug = require('gulp-pug'),
     plumber = require('gulp-plumber'),
     livereload = require('gulp-livereload');
@@ -23,12 +28,53 @@ gulp.task('html-build', function buildHTML() {
         pretty: '    ',
     }))
     .pipe(gulp.dest('public/'))
-    .pipe(livereload({ start: true }));
 });
 
 
+// Style
+gulp.task('demo-style', function styleDemo() {
+    return gulp.src([
+            '__dev/scss/normalize.scss',
+            '__dev/scss/fonts.scss',
+            '__dev/scss/base.scss',
+            '__dev/scss/typography.scss',
+            '__dev/scss/layouts.scss',
+            '__dev/scss/modules.scss',
+            '__dev/scss/pages.scss'
+        ])
+    .pipe(plumber())
+    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(concatCss('style.min.css'))
+    .pipe(autoprefixer())
+    .pipe(gcmq())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('demo/assets/css'))
+    .pipe(livereload({ start: true }));
+});
+
+// Style
+gulp.task('css-build', function styleDemo() {
+    return gulp.src([
+            '__dev/scss/normalize.scss',
+            '__dev/scss/fonts.scss',
+            '__dev/scss/base.scss',
+            '__dev/scss/typography.scss',
+            '__dev/scss/layouts.scss',
+            '__dev/scss/modules.scss',
+            '__dev/scss/pages.scss'
+        ])
+    .pipe(plumber())
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gcmq())
+    // .pipe(cleanCSS({
+    //     format: 'beautify'
+    // }))
+    .pipe(gulp.dest('public/assets/css'))
+});
+
 // Build
-gulp.task('build', gulp.series('html-build'));
+gulp.task('build', gulp.series('html-build','css-build'));
 
 
 // Default
@@ -36,4 +82,6 @@ gulp.task('default', function () {
     livereload.listen();
 
     gulp.watch('__dev/templates/**/*.pug', gulp.series('html-demo'));
+
+    gulp.watch('__dev/scss/**/*.scss', gulp.series('demo-style'));
 });
